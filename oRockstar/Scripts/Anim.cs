8a5c7cc2e4c7e5e295a5a00b3c55951ad3179300
Timxu2016 <xuwei_buaa@163.com>
2017-02-13 13:39:16 +0800

    Add stars and metero spawner to orockstar scene

---
+++ oRockstar/Scripts/Anim.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anim : MonoBehaviour 
+{
+    public float speed = 4f;
+    private float time;
+
+	void Update ()
+    {
+        time += Time.deltaTime;
+        transform.position += speed * Time.deltaTime * transform.forward;
+        if (time > 20f)
+            DestroyImmediate(this.gameObject);
+	}
+}
---
+++ oRockstar/Scripts/MeteroSpawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteroSpawn : MonoBehaviour
+{
+    public GameObject[] metero;
+    public float time;
+
+    void Awake()
+    {
+        for(int i=0;i<3;i++)
+            Instantiate(metero[i]);
+    }
+
+	void Update () 
+    {
+        time += Time.deltaTime;
+        if (time >= 30f)
+        {
+            for(int i=0;i<3;i++)
+                Instantiate(metero[i]);
+            time = 0;
+        }
+	}
+}
